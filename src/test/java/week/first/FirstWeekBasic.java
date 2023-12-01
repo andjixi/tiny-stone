b8fe2abefaa21ad1d058ff0c5468344fac9f152b
Anđela Jovanović <andjela_07@hotmail.com>
2023-12-01 17:08:18 +0100

    clean up FirstWeekBasic.java
    I've edited the file and improved its structure.

--- src/test/java/week/first/FirstWeekBasic.java
+++ src/test/java/week/first/FirstWeekBasic.java
@@ -9,14 +9,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 import java.time.Duration;
+
+
 
 public class FirstWeekBasic {
 
     @Test
-    public void firstTest(){
-
+    public void firstTestSuccess(){
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
@@ -25,9 +27,6 @@ public class FirstWeekBasic {
 
         WebElement usernameFieldID = driver.findElement(By.id("user-name"));
         usernameFieldID.sendKeys("standard_user");
-
-        WebElement usernameFieldName = driver.findElement(By.name("user-name"));
-        usernameFieldName.sendKeys("standard_user");
 
         WebElement passwordFieldID = driver.findElement(By.id("password"));
         passwordFieldID.sendKeys("secret_sauce");
@@ -43,7 +42,34 @@ public class FirstWeekBasic {
     }
 
     @Test
-    public void secondTest(){
+    public void firstTestFail(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement usernameFieldID = driver.findElement(By.id("user-name"));
+        usernameFieldID.sendKeys("locked_out_user");
+
+        WebElement passwordFieldID = driver.findElement(By.id("password"));
+        passwordFieldID.sendKeys("secret_sauce");
+
+        WebElement loginButtonID = driver.findElement(By.id("login-button"));
+        loginButtonID.click();
+
+        //unsuccessful login
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("inventory.html"));
+
+        // check if the current URL is still the login page
+        Assert.assertTrue(driver.getCurrentUrl().contains("https://www.saucedemo.com/"));
+
+        driver.quit();
+    }
+
+
+    @Test
+    public void secondTestSuccess(){
 
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver = new FirefoxDriver();
@@ -58,58 +84,86 @@ public class FirstWeekBasic {
         System.out.println("CURRENT URL IS -> " + currentURL);
 
         WebElement usernameFieldXPath = driver.findElement(By.xpath("//input[@type='text']"));
-        usernameFieldXPath.sendKeys("nevalidan_korisnik");
+        usernameFieldXPath.sendKeys("standard_user");
 
 
         WebElement usernameFieldCSS = driver.findElement(By.cssSelector("input[type='password']"));
-        usernameFieldCSS.sendKeys("pogresna_lozinka");
+        usernameFieldCSS.sendKeys("secret_sauce");
 
         WebElement logInButtonCSS = driver.findElement(By.cssSelector("input[value='Login']"));
         logInButtonCSS.click();
 
-        WebDriverWait wait =
-                new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h3[data-test='error']")));
-
-//        wait.until(ExpectedConditions.titleIs("Swag Labs"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleIs("Swag Labs"));
 
 
-        currentURL =
-                driver.getCurrentUrl();
-
+        currentURL = driver.getCurrentUrl();
         System.out.println("CURRENT URL IS -> " + currentURL);
 
         WebElement menu = driver.findElement((By.id("react-burger-menu-btn")));
         menu.click();
 
-        WebElement logOutButton =
-                driver.findElement(By.id("logout_sidebar_link"));
-
+        WebElement logOutButton = driver.findElement(By.id("logout_sidebar_link"));
         logOutButton.click();
 
-        currentURL =
-                driver.getCurrentUrl();
+        currentURL = driver.getCurrentUrl();
+        System.out.println("CURRENT URL IS -> " + currentURL);
 
+        driver.close();
+    }
+
+    @Test
+    public void secondTestFail(){
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://www.saucedemo.com/");
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("CURRENT URL IS -> " + currentURL);
+
+        WebElement usernameFieldXPath = driver.findElement(By.xpath("//input[@type='text']"));
+        usernameFieldXPath.sendKeys("error_user");
+
+        WebElement usernameFieldCSS = driver.findElement(By.cssSelector("input[type='password']"));
+        usernameFieldCSS.sendKeys("secret_sauce");
+
+        WebElement logInButtonCSS = driver.findElement(By.cssSelector("input[value='Login']"));
+        logInButtonCSS.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.titleIs("Swag Labs"));
+
+
+        currentURL = driver.getCurrentUrl();
+        System.out.println("CURRENT URL IS -> " + currentURL);
+
+        WebElement menu = driver.findElement((By.id("react-burger-menu-btn")));
+        menu.click();
+
+        WebElement logOutButton = driver.findElement(By.id("logout_sidebar_link"));
+        logOutButton.click();
+
+        currentURL = driver.getCurrentUrl();
         System.out.println("CURRENT URL IS -> " + currentURL);
 
         driver.close();
     }
 
 
-    /*
-    Thread.sleep(10000);
+//    Thread.sleep(10000);
+//
+//
+//
+//    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+//        wait.until(
+//                ExpectedConditions.visibilityOfElementLocated(
+//                By.xpath("//div[@class='alert alert-success text-center']")
+//                ));
 
-
-
-
-
-
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-        wait.until(
-                ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//div[@class='alert alert-success text-center']")
-                ));
-    */
 
 }
 
